18e57f7369841cbf755e5343cadd1e3bc1fbf9a6
Daniel Fraga <dan.fraga33@gmail.com>
2023-11-05 15:26:44 +0000

    chore: delete - moved to src dir

--- components/Applayout/types/TAppLayoutProps.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode, ReactElement } from 'react';
-import { NextPage } from 'next';
-import { AppProps } from 'next/app';
-
-export type LayoutProps = {
-	children: ReactNode;
-};
--- pages/Types/LayoutTypes.ts
+++
@@ -1,8 +0,0 @@
-import React, { FC, ComponentType, ReactElement, ReactNode } from 'react';
-import { NextPage } from 'next';
-
-export type Page<P = {}> = NextPage<P> & {
-	// You can disable whichever you don't need
-	getLayout?: (page: ReactElement) => ReactNode;
-	layout?: ComponentType;
-};
