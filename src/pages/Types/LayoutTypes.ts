4e4d23ce0b9c2a92a9f007b43058ed30495f2f12
Daniel Fraga <dan.fraga33@gmail.com>
2023-11-14 14:18:54 +0000

    chore(package): updating packages

--- src/pages/Types/LayoutTypes.ts
+++
@@ -1,8 +0,0 @@
-import { ComponentType, ReactElement, ReactNode } from "react";
-import { NextPage } from "next";
-
-export type Page<P = {}> = NextPage<P> & {
-  // You can  whichever you don't need
-  getLayout?: (page: ReactElement) => ReactNode; // eslint-disable-line
-  layout?: ComponentType;
-};
