a99c6d65add68ded4284ba2618cd28a509e0b17a
Alexandre Pereira <alexandre.carneiro.pereira@gmail.com>
2023-11-18 19:00:52 +0000

    fix: environment variables for stripe

--- src/pages/api/addTokens.ts
+++ src/pages/api/addTokens.ts
@@ -1,13 +1,21 @@
 import stripeInit from "stripe";
 import { getAuth } from "@clerk/nextjs/server";
-const stripe = new stripeInit(process.env.STRIPE_KEY as string);
+
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error("Missing Stripe environment variables");
+}
+
+const stripe = new stripeInit(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
+  if (!process.env.STRIPE_PRODUCT_PRICE_ID) {
+    res.status(500).json({ error: "Internal error" });
+    throw new Error("Missing STRIPE_PRODUCT_PRICE_ID environment variables");
+  }
+
   const { userId } = getAuth(req);
   const lineItems = [
     {
-      // price: process.env.STRIPE_PRODUCT_PRICE_ID,
-      // quantity: 1,
       price: process.env.STRIPE_PRODUCT_PRICE_ID,
       quantity: 1,
     },
